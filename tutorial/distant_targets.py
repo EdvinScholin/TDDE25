d8ba19f1243994e6d2f5733533a4132975daa6ba
ponmo676 <ponmo676@tlhw-5-3.ad.liu.se>
2020-11-05 13:36:46 +0100

    added tutorial folder

---
+++ tutorial/distant_targets.py
@@ -0,0 +1,117 @@
+#
+# This file can be used as a starting point for the bots.
+#
+
+import sys
+import traceback
+import math
+import libpyAI as ai
+from optparse import OptionParser
+
+#
+# Global variables that persist between ticks
+#
+tickCount = 0
+mode = "wait"
+targetId = -1
+
+def tick():
+    #
+    # The API won't print out exceptions, so we have to catch and print them ourselves.
+    #
+    try:
+
+        #
+        # Declare global variables so we have access to them in the function
+        #
+        global tickCount
+        global mode
+        global targetId
+
+        #
+        # Reset the state machine if we die.
+        #
+        if not ai.selfAlive():
+            tickCount = 0
+            mode = "wait"
+            return
+
+        tickCount += 1
+
+        #
+        # Read some "sensors" into local variables, to avoid excessive calls to the API
+        # and improve readability.
+        #
+
+        selfX = ai.selfX()
+        selfY = ai.selfY()
+        selfVelX = ai.selfVelX()
+        selfVelY = ai.selfVelY()
+        selfSpeed = ai.selfSpeed()
+
+        selfHeading = ai.selfHeadingRad() 
+        # 0-2pi, 0 in x direction, positive toward y
+
+        # Add more sensors readings here
+        targetCount = ai.targetCountServer()
+        targetCountAlive = 0
+
+        for i in range(targetCount):
+            if ai.targetAlive(i):
+                targetCountAlive += 1
+
+        print ("tick count:", tickCount, "mode:", mode, "targets alive:", targetCountAlive)
+
+
+        if mode == "wait":
+            if targetCountAlive > 0:
+                mode = "aim"
+
+        elif mode == "aim":
+            if targetCountAlive == 0:
+                mode = "wait"
+                return
+
+
+            for target in range(targetCount):
+                if ai.targetAlive(target):
+                    targetId = target
+
+
+            x = ai.targetX(targetId) - selfX
+            y = ai.targetY(targetId) - selfY
+
+            targetDirection = math.atan2(y, x)
+
+
+
+            ai.turnToRad(targetDirection)
+
+
+    except:
+        print(traceback.print_exc())
+
+
+#
+# Parse the command line arguments
+#
+parser = OptionParser()
+
+parser.add_option ("-p", "--port", action="store", type="int", 
+                   dest="port", default=15348, 
+                   help="The port number. Used to avoid port collisions when" 
+                   " connecting to the server.")
+
+(options, args) = parser.parse_args()
+
+name = "Stub"
+
+#
+# Start the AI
+#
+
+ai.start(tick,["-name", name, 
+               "-join",
+               "-turnSpeed", "64",
+               "-turnResistance", "0",
+               "-port", str(options.port)])
