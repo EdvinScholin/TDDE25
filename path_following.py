49ae8e525e558a7be878d7ea12bde88da39d237c
edvsc779 <edvsc779@tlhw-4-1.ad.liu.se>
2020-11-20 11:57:45 +0100

    Gjort lite kommentarer i filen för att se vad vilka problem vi kommer stöta på.

---
+++ path_following.py
@@ -0,0 +1,10 @@
+# Vi vill använda basic navigation fast inte använda en straight line navigation,
+# förslagsvis använda wallfeeler för att unvdika hinderna. En path ska ta hänsyn till att vingarna kan åka in i väggar och
+# narrow corridors.
+# Path drawing var bara för att vi skulle se hur den skrev ut pathen eller ska vi använda den?
+
+# Vi kanske kan använda power / (mass+5) för att hantera friktionen på kartan, hur ska vi få tillgång till olika kartors friktion?
+# Hantera skott? Använda sköld och motverka kraften man får? Hur är det menat att det ska fungera? Kan använda 
+# item collection för att spara ett skotts id då det ädnras varje tick.
+# Hur ska vi åka bort från andra skepp som rör sig?
+# Använd oss Markus navigation av skepp för att ta oss fram så snabbt som möjligt.
