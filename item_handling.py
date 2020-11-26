2111d9ad3f8b1e4c8a26bd93826a91df6d22936b
Markus Handstedt <marha066@student.liu.se>
2020-11-26 16:13:05 +0100

    gsakjh

--- item_handling.py
+++ item_handling.py
@@ -116,7 +116,7 @@ def tick():
         except ZeroDivisionError:
             power = 55
 
-        if 5 <= power <= 55:
+        if 40 <= power <= 55:
             if wallDistance < 80:
                 power = 55
             mode = "stop"
