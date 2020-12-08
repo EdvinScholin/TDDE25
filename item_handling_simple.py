63b4048ab37e2144b098b6571a3d10c2cc3dadd0
Markus Handstedt <marha066@student.liu.se>
2020-12-08 23:02:40 +0100

    simplified item handling

--- item_handling_simple.py
+++ item_handling_simple.py
@@ -111,17 +111,17 @@ def tick():
         # ----------------------------------------------------------------------------
 
         if lib.brake(wallDistance - 50) and wallDistance != -1:
+            '''
             prevTrackRad = ai.selfTrackingRad()
             mode = "stop"
             print("wallFeeler")
-
             '''
+
             ai.turnToRad(ai.selfTrackingRad() - math.pi)
             print("self heading", ai.selfHeadingRad())
             ai.setPower(55)
             ai.thrust()
-            mode = "ready"
-            '''
+            return
 
         # ---------------------------------------------------------------------------
         # Ready
