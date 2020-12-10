2406b82e9fed2a77b43989b3834845cd599c56a8
Markus Handstedt <marha066@student.liu.se>
2020-12-10 14:38:13 +0100

    hanterar stop

--- edvin_copy_item_handling.py
+++ edvin_copy_item_handling.py
@@ -112,7 +112,7 @@ def tick():
         # Wallfeeler
         # ----------------------------------------------------------------------------
 
-        if lib.brake(wallDistance - 100) and wallDistance != -1 and:
+        if lib.brake(wallDistance - 100) and wallDistance != -1:
             prevTrackRad = selfTrackingRad
             print("wallfeeler")
             mode = "stop"
